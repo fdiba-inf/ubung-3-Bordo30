5b54da690b93775958b02c7d6ed5ca6d76619ea9
Bordo30 <boris.dobrev30@gmail.com>
2021-10-28 09:34:30 +0300

    Just trying!

--- src/exercise3/NumberSearch.java
+++ src/exercise3/NumberSearch.java
@@ -5,12 +5,19 @@ public class NumberSearch {
         int numberCounter = 0;
         for(int number = 101; number>100 && number<1000; number++) {
             if(number%5==0 && number%6==0) {
-                System.out.print(number + " ");
                 numberCounter++;
-                if(numberCounter == 10 || numberCounter == 20) {
-                    System.out.println();
-                } else if (numberCounter==30) {
-                    System.out.print("\n");
+                if(numberCounter<10) {
+                    System.out.print(number + " ");
+                } else if(numberCounter == 10) {
+                    System.out.print(number + "\n");
+                } else if(numberCounter < 20) {
+                    System.out.print(number + " ");
+                } else if(numberCounter == 20) {
+                    System.out.print(number + "\n");
+                } else if(numberCounter < 30) {
+                    System.out.print(number + " ");
+                } else if(numberCounter == 30) {
+                    System.out.print(number);
                 }
             }
 
